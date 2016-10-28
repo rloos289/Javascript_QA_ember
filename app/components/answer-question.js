d0cb066f7e32821d62ce5be20bdccd0c31d05a38
Ryan Loos <RLoos289@gmail.com>
2016-10-28 16:00:34 -0700

    fix show error

--- app/components/answer-question.js
+++ app/components/answer-question.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // showDetails: false,
   actions: {
     switchToUpdate(question) {
       this.sendAction('switchToUpdate', question);
@@ -13,6 +14,9 @@ export default Ember.Component.extend({
     },
     deleteQuestion(question) {
       this.sendAction('deleteQuestion', question);
-    }
+    },
+    showDetails(question) {
+      this.toggleProperty('showDetails', question);
+    },
   }
 });
--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,11 +3,17 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   showDetails: false,
   actions: {
+    switchToUpdate(question) {
+      this.sendAction('switchToUpdate', question);
+    },
     toggleAnswer(question) {
       this.sendAction('toggleAnswer', question);
     },
     deleteAnswer(answer) {
       this.sendAction('deleteAnswer', answer);
+    },
+    deleteQuestion(question) {
+      this.sendAction('deleteQuestion', question);
     },
     showDetails(question) {
       this.toggleProperty('showDetails', question);
