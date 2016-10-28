3ffe41dff822f7d45c5ae729c42e2c3e5fd0c21c
Ryan Loos <RLoos289@gmail.com>
2016-10-28 09:00:25 -0700

    add ability to trigger update form

--- app/components/answer-question.js
+++ app/components/answer-question.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    switchToUpdate(question) {
+      this.sendAction('switchToUpdate', question);
+    }
+  }
 });
--- app/components/display-questions.js
+++ app/components/display-questions.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  currentQuestion: null,
+  switchToUpdate: false,
   actions: {
     saveQuestion(params) {
       this.sendAction('saveQuestion', params);
+    },
+    switchToUpdate(question) {
+      this.set('switchToUpdate', true);
+      this.set('currentQuestion', question);
     }
   }
 });
