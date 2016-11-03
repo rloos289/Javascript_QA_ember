6f65e5bc28044a2a056587882a23697c68aba2b6
Ryan Loos <ryan@>
2016-11-03 15:17:10 -0700

    fixed star rating

--- app/helpers/answer-rating.js
+++ app/helpers/answer-rating.js
@@ -3,17 +3,18 @@ import Ember from 'ember';
 export function answerRating(params) {
   var ratingAverage = params[0].get('rating');
 
-  if (ratingAverage > 4) {
-    return "*****";
-  } else if (ratingAverage >= 3) {
-    return "****";
-  } else if (ratingAverage >= 2) {
-    return "***";
-  } else if (ratingAverage >= 1) {
-    return "**";
-  } else {
-    return "*";
+  var stars = "";
+  for (var i = 0; i < ratingAverage; i++) {
+    stars += '<span class="glyphicon glyphicon-star "></span>'
   }
+  return Ember.String.htmlSafe(stars);
 }
+//make half stars or use 10 star system
+//fix math
+//dynamic updating
+//fix feedback (empty objects coming in)
+//sort by top rated answer
+//average resets after refresh save them in db as empty array 
+
 
 export default Ember.Helper.helper(answerRating);
